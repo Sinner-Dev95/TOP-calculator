5323cf99223e03696dbdc1c9159bf3b1201fd59f
sinnerdev-- <Sinner-Dev95@users.noreply.github.com>
2025-05-31 16:39:51 +0200

    Dom manipulation of calculator, dispolay and buttons, create three variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('Hello World!');
+//manipolazione elementi html calcolatrice
+const calcolatrice = document.querySelector('.calcolatrice-container');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
